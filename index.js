f6db988ab528cc8b24c009a4c0259ba9330fd24b
Rajesh R <rajeshrpm01@gmail.com>
2024-05-12 11:46:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const math = require("math");
+
+// SQRT function
+function squareRoot(num) {
+  return math.sqrt(num);
+}
+
+const number = 36;
+const result = squareRoot(number);
+console.log(result);
